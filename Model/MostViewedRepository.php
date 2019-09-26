3b14e2d0f105364c757c8fcc10854177d36b7d28
Dmytro Portenko <troublesohard@gmail.com>
2019-09-26 15:43:51 +0300

    add all attributes to select

--- Model/MostViewedRepository.php
+++ Model/MostViewedRepository.php
@@ -49,23 +49,17 @@ class MostViewedRepository {
     public function getList($limit = 10)
     {
         $storeId = $this->storeManager->getStore()->getId();
-        $collection = $this->productsFactory->create()->addAttributeToSelect(
-            'entity_id',
-            'views'
-        )->addAttributeToSelect(
-            'name'
-        )->addViewsCount()->setStoreId(
-            $storeId
-        )->setVisibility(
-            [
-                \Magento\Catalog\Model\Product\Visibility::VISIBILITY_IN_CATALOG,
-                \Magento\Catalog\Model\Product\Visibility::VISIBILITY_BOTH
-            ]
-        )->addStoreFilter(
-            $storeId
-        )->setPageSize(
-            $limit
-        );
+        $collection = $this->productsFactory->create()
+            ->addAttributeToSelect('*')
+            ->addViewsCount()->setStoreId($storeId)
+            ->setVisibility(
+                [
+                    \Magento\Catalog\Model\Product\Visibility::VISIBILITY_IN_CATALOG,
+                    \Magento\Catalog\Model\Product\Visibility::VISIBILITY_BOTH
+                ]
+            )
+            ->addStoreFilter($storeId)
+            ->setPageSize($limit);
 
         // TODO: add cache here for products as in ProductRepository
 
